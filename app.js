b5a2f11e0bc176584539c3062bb5d0d6656f0ea3
Artem Titoulenko <artem.titoulenko@gmail.com>
2013-04-18 20:01:08 -0400

    authentication system is in place

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express')
   , routes = require('./routes')
-  , room = require('./routes/room')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , _ = require('underscore')
+  , passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
 
 var app = express()
   , server = http.Server(app)
@@ -17,8 +19,10 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
+app.use(express.cookieParser());
+app.use(express.session({secret: '460ddf6eb1ea6353dedefbb435067e6a5a46ba4b'}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,8 +31,49 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+app.set('users'
+  , [
+      { password: 'foo', email: 'artem@obvious.com', id: 1 }
+    , { password: 'bar', email: 'maltz@yelp.com', id: 2}
+    , { password: 'baz', email: 'grardb@etsy.com', id: 3}
+])
+
+// Passport configuration
+passport.use(new LocalStrategy(
+  function (email, password, done) {
+    console.log('checking ' + email + ', ' + password)
+    var user = _.findWhere(app.get('users'), {email: email, password: password})
+
+    if ( _.isObject(user) ) {
+      done(null, user)
+    } else {
+      done(null, false, { message: 'That email/password combination doens\'t seem to exist' })
+    }
+  }
+))
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id)
+})
+
+passport.deserializeUser(function(id, done) {
+  var user = _.findWhere(app.get('users'), {id: id})
+  console.log('found user ')
+  console.dir(user)
+  done(null, user)
+})
+
+// Actual routes
 app.get('/', routes.index);
-app.get('/room', room.index);
+app.get('/room', ensureAuthenticated, routes.room);
+
+app.get('/login', routes.login)
+app.post('/login'
+  , passport.authenticate(  'local'
+                          , { failureRedirect: '/login' })
+                          , function (req, res) {
+                            res.redirect('/room')
+                          })
 
 var chat = io
   .of('/chat')
@@ -41,3 +86,8 @@ var chat = io
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index')
+}
+
+exports.login = function (req, res) {
+  res.render('login', {})
+}
+
+exports.room = function (req, res) {
+  console.log('user: ' + req.user)
+  var user = req.user
+
+  res.render('room', {user: req.user})
 }
